560e18154e1e4513225bec6c8bd34f6f4f900d54
Liansai <aquadong0227@gmail.com>
2017-12-04 01:07:33 -0500

    delete the keys and secrets
    What was I thinking!?

--- tweepyOAuth.py
+++ tweepyOAuth.py
@@ -3,10 +3,10 @@
 import tweepy #https://github.com/tweepy/tweepy
 import csv
 
-consumer_key = "EoV50LfF854f2DFPr6FGpQq26"
-consumer_secret = "J2rDWovgDHy7Xa4IlFJ0M8AGGzN4f8458V4yQnOE0GfVtrcFaV"
-access_key = "918550319726059526-9qFOpZBPDCn0xlV7fF0iEYj4uahdTCf"
-access_secret = "zsFhlIfkJsysXsLfJzBreerrOl9VFJ1Koyc3QuVnQaCQz"
+consumer_key = "XXX"
+consumer_secret = "XXX"
+access_key = "XXX"
+access_secret = "XXX"
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
