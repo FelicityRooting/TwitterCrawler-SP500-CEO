cacbe1198b3e0781e70e25d43d1f941f8ea0a2b7
Felicity Liu <FelicityRooting@users.noreply.github.com>
2017-12-06 00:33:30 -0500

    some small modify

--- tweepyOAuth.py
+++ tweepyOAuth.py
@@ -1,12 +1,15 @@
 # twitter oauth test
 
+#用Tweepy抓取twitter数据
 import tweepy #https://github.com/tweepy/tweepy
 import csv
 
-consumer_key = "XXX"
-consumer_secret = "XXX"
-access_key = "XXX"
-access_secret = "XXX"
+
+#Twitter API credentials
+consumer_key = "SAHnTZbkW1B3gCR7l4hBPapVR"
+consumer_secret = "UMXUFeiYAddSQAFjPP10VX9eDb40QSSlVLAF1f6VFPz2G5oM3j"
+access_key = "918549121853779968-r0WlJS4XWhuHL9ReLLhcflFuMZQ7B4f"
+access_secret = "6eSW5CM8HSHTeRtAPHCSVziM3WbbjiRoz4vgFh8hFJ3kG"
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
